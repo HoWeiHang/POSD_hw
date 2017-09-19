52f101a22ea7158fe3ac472e5aab2a9f763bcf29
HoWeiHang <wkite2001@gmail.com>
2017-09-19 23:00:07 +0800

    Add: five unit tests in utSort

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  POSDHomework
+//
+//  Created by Ｗillie on 9/19/17.
+//  Copyright © 2017 Ｗillie. All rights reserved.
+//
+
+#include <gtest/gtest.h>
+#include "utSort.h"
+
+int main( int argc , char **argv )
+{
+    testing :: InitGoogleTest( &argc , argv );
+    return RUN_ALL_TESTS( ) ;
+}
